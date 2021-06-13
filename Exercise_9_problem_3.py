e4beb99f980630a7a1a64e6faaf93017f196052a
RentaroSuzuki <82135239+RentaroSuzuki@users.noreply.github.com>
2021-06-13 17:56:52 +0000

    Ex 9_3 L49~L50 I add codes of to set movements

--- Exercise_9_problem_3.py
+++ Exercise_9_problem_3.py
@@ -46,7 +46,9 @@ assert len(grouped.groups) == data["userid"].nunique(), "Number of groups should
 # YOUR CODE HERE 4 to set movements
 import pandas as pd
 from shapely.geometry import LineString, Point
-movements=None
+movements=gpd.GeoDataFrame(columns=['userid','geometry'])
+movements=data.to_crs(epsg=32735)
+
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check the result
